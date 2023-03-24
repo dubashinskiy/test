0df57ec13f3c699cc463ca57cc81e02ab8f5108c
Vitaliy Dubashinskiy <dubashinskiiv@vtb.ru>
2023-03-24 16:54:52 +0300

    some commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,14 +3,8 @@ import java.math.BigDecimal;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println(TestUtils.getParams());
-
-        StringBuilder pm = new StringBuilder();
-        String str = pm.toString();
-        str = str + "qaz";
-        System.out.println(str + "qwe");
-        
         String main = "qwe";
 
     }
+
 }
